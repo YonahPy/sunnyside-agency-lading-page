f91cbb7a43e0c6ac0c89a4fefcab30acac9bc850
YonahPy <jrjonas487@gmail.com>
2023-10-03 12:06:22 -0300

    html

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors:{
+      red: '#FF4D4D',
+      yellow: '#FFD700',
+      darkGreen: '#194D19',
+      darkBlue: '#1A2E4A',
+      lightGreen: '#66CC66',
+      desaturedBlue: 'hsl(212, 27%, 19%)',
+      grayishBlue: 'hsl(213, 9%, 39%)',
+      lightGrayishBlue: 'hsl(232, 10%, 55%)',
+      lightBlue: 'hsl(210, 4%, 67%)',
+      white: 'hsl(0, 0%, 100%)'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
